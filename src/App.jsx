28a2071817e8e125b8c31dbda9884937c0955aa6
Arpit <arpit@interfacem.com>
2020-08-27 01:57:58 +0530

    - Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+const api = {
+    key : '2f582f6dc33d10e25717fad99b5b9394',
+    base : 'api.openweathermap.org/data/2.5/'
+};
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Hello Arpit!
+
+            </div>
+        );
+    }
+}
+
+export default App;
